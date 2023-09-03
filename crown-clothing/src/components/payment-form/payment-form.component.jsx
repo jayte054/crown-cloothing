d8ee1b57dbbf0c457d310550ee11845e38511ac2
Justin Ewelike <54476858+jayte054@users.noreply.github.com>
2023-09-03 11:21:35 +0100

    netlify function

--- crown-clothing/src/components/payment-form/payment-form.component.jsx
+++ crown-clothing/src/components/payment-form/payment-form.component.jsx
@@ -6,12 +6,21 @@ const PaymentForm = () => {
     const stripe = useStripe()
     const element = useElements()
 
-    const paymentHandler = () => {
+    const paymentHandler = async (e) => {
+
+        e.preventDefault()
 
         if(!stripe || !element) {
             return;
         }
-
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({amount: 1000})
+        }).then(res => res.json())
+        console.log(response)
     }
 
     return (
@@ -19,7 +28,7 @@ const PaymentForm = () => {
             <div className="form-container">
                 <h2>Credit Card Payment</h2>
             <CardElement />
-            <Button buttonType={BUTTON_TYPES_CLASSES.inverted} onClick={paymentHandler}>Pay Now</Button>
+            <Button buttonType={BUTTON_TYPES_CLASSES.inverted} onClick={(e) => paymentHandler(e)}>Pay Now</Button>
             </div>
 
         </div>
