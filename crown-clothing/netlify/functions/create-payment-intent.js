69e6409a2d67b631b71bfff1663c687d41cb3a18
Justin Ewelike <54476858+jayte054@users.noreply.github.com>
2023-09-05 02:14:50 +0100

    netlify function

--- crown-clothing/netlify/functions/create-payment-intent.js
+++ crown-clothing/netlify/functions/create-payment-intent.js
@@ -2,20 +2,30 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = async(e) => {
+   console.log("amount")
+   console.log(e)
    try{
+    console.log("amount")
     const {amount} = JSON.parse(e.body)
-
     const paymentIntent = await stripe.paymentIntents.create({
         amount,
         currency : "usd",
         payment_method_types: ["card"]
     })
-    return {
+    const response = {
         statusCode: 200,
-        body: JSON.stringify({paymentIntent})
-    }
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ paymentIntent }),
+      };
+      return response;
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({paymentIntent})
+    // }
    }catch (error) {
-    console.log({error})
+    console.log("this is the error", {error})
 
     return {
         statusCode: 400,
--- crown-clothing/src/components/payment-form/payment-form.component.jsx
+++ crown-clothing/src/components/payment-form/payment-form.component.jsx
@@ -1,26 +1,36 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import  {Button, BUTTON_TYPES_CLASSES}  from "../button/button.component"
 import "./payment-form.style.scss"
+// import "../../../"
 
 const PaymentForm = () => {
-    const stripe = useStripe()
-    const element = useElements()
+    const stripe = useStripe();
+    const element = useElements();
 
     const paymentHandler = async (e) => {
-
-        e.preventDefault()
-
+        console.log("payment")
+        e.preventDefault();
+        console.log("pay")
         if(!stripe || !element) {
             return;
         }
-        const response = await fetch("/.netlify/functions/create-payment-intent", {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({amount: 1000})
-        }).then(res => res.json())
-        console.log(response)
+        console.log("return")
+
+        try{
+            console.log("response")
+            const response = await fetch('../../../.netlify/functions/create-payment-intent.js', {
+                statuscode: 200,
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({amount: 1000})
+            }).then(res => res.json());
+            console.log(response)
+        }catch(error){
+            console.log("this is the error",{error})
+        }
+        
     }
 
     return (
@@ -28,7 +38,7 @@ const PaymentForm = () => {
             <div className="form-container">
                 <h2>Credit Card Payment</h2>
             <CardElement />
-            <Button buttonType={BUTTON_TYPES_CLASSES.inverted} onClick={(e) => paymentHandler(e)}>Pay Now</Button>
+            <Button type="button" buttonType={BUTTON_TYPES_CLASSES.inverted} onClick={ paymentHandler }>Pay Now</Button>
             </div>
 
         </div>
